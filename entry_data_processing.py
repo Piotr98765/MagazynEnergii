8cd6de0aedbe43184ef37f599bc407a4868e97c9
Piotr98765 <61555492+PiotrCheski@users.noreply.github.com>
2023-09-22 13:28:41 +0200

    Update entry_data_processing.py

--- entry_data_processing.py
+++ entry_data_processing.py
@@ -7,6 +7,7 @@ HOLIDAY_MULTIPLIER = 0.1
 def extend_energy_consumption(original_list, hours):
     return list(itertools.islice(itertools.cycle(original_list), hours))
 
+# Function to calculate energy consumption for holiday hours
 def holiday_energy_consumption(origina_list):
     return [x * HOLIDAY_MULTIPLIER for x in origina_list]
 
